482ac889c74beaf18139e0d0037420fb27b29082
Akshay Pandhare <akshaypandhare@XINMBMAC0HKY6.local>
2025-01-17 12:20:17 +0530

    Added cicd pipeline

--- Python/app.py
+++ Python/app.py
@@ -49,7 +49,7 @@ def get_counter():
     cur.close()
     conn.close()
 
-    return f"Current Counter Value: {new_count}"
+    return f"Akshay Current Counter Value: {new_count}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
